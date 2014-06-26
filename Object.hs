236661daf77f9578fcc29ec34eb8f89010184ff4
Anthony Burzillo <burz@jhu.edu>
2014-06-25 22:12:35 -0400

    Make Object an instance of Intersectionable

--- Object.hs
+++ Object.hs
@@ -7,19 +7,26 @@ module Object
 ) where
 
 import Math.Sphere
+import Math.Intersection
 
 import Control.Lens
 
 data Object' a = Object { _obj :: a }
 
+makeLenses ''Object'
+
+instance Intersectable a => Intersectable (Object' a) where
+    intersect r o = intersect r $ o ^. obj
+
 data ObjectType =
-      Sphere Sphere
+      Sphr Sphere
+
+instance Intersectable ObjectType where
+    intersect r (Sphr s) = intersect r s
 
 type Object = Object' ObjectType
 
 object :: ObjectType -> Object
 object o = Object { _obj = o
                   }
-
-makeLenses ''Object'
 
